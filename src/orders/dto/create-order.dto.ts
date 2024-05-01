c14e8bc1ed0f2279697b7e1eef5a6ecbfd79636c
osman.ozts <osman.ozts@gmail.com>
2024-05-01 23:19:42 +0200

    added is not empty validation pipe for order table number

--- src/orders/dto/create-order.dto.ts
+++ src/orders/dto/create-order.dto.ts
@@ -1,6 +1,9 @@
 import { OrderItem } from '../order.model';
+import { IsNotEmpty } from 'class-validator';
 
 export class CreateOrderDto {
   items: OrderItem[];
+
+  @IsNotEmpty()
   tableNumber: number;
 }
